eae7e51690159dfe2aa662c6028c4654e8bd1b2b
jlmooreiii <jloftonmooreiii@gmail.com>
2020-09-02 15:13:00 -0700

    Started building out pages and got Devise views up

---
+++ app/javascript/components/Pages/ProjectNew.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class ProjectNew extends Component {
+    render() {
+        return(
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+export default ProjectNew
