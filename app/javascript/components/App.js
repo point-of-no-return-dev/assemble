55631d76d66962aaa7911cf7c47f10fe8f0936ca
jlmooreiii <jloftonmooreiii@gmail.com>
2020-09-03 14:15:36 -0700

    Having issues with rendering the pages after creating the routes in App js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import Header from "./Components/Header.js"
 import Footer from "./Components/Footer.js"
+import Landing from "./Pages/Landing.js"
+import Index from "./Pages/Index.js"
+import ProjectNew from "./Pages/ProjectNew.js"
+import ProjectShow from "./Pages/ProjectShow.js"
+import ProjectEdit from "./Pages/ProjectEdit.js"
 import {
   BrowserRouter as Router,
   Route,
@@ -25,6 +30,60 @@ class App extends React.Component {
           sign_in_route={ sign_in_route }
           sign_out_route={ sign_out_route }
         />
+        <Switch>
+          <Route
+            exact path="/"
+            render={ (props) =>
+              <Landing
+                logged_in={ logged_in }
+                sign_in_route={ sign_in_route }
+                sign_out_route={ sign_out_route }
+              />
+            }
+          />
+
+          <Route
+            path="/index"
+            render={ (props) =>
+              <Index
+                logged_in={ logged_in }
+                sign_in_route={ sign_in_route }
+                sign_out_route={ sign_out_route }
+                // Update State
+              />
+            }
+          />
+          <Route
+            path={"/project_show/:id"}
+            render={ (props) => {
+              let id = props.match.params.id
+              let project = this.state.projects.find(project => project.id === parseInt(id))
+              return (
+                <ProjectShow project = {project}/>
+              )
+            }}
+          />
+          <Route
+            path={"/project_edit/:id"}
+            render={ (props) => {
+              let id = props.match.params.id
+              let project = this.state.projects.find(project => project.id === parseInt(id))
+              return (
+                <ProjectEdit project = {project}/>
+              )
+            }}
+          />
+          <Route
+            path="/project_new"
+            render={ (props) =>
+              <ProjectNew
+                //Make a createNewProject method to pass as a prop
+                current_user={current_user}
+                // Update State
+              />
+            }
+          />
+        </Switch>
         <Footer />
       </Router>
     );
