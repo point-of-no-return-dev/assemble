a63784b60c5ac5c56e94337ed262098410d52b20
jlmooreiii <jloftonmooreiii@gmail.com>
2020-09-04 15:42:45 -0700

    Filter method in App.js is done and conditional render in index is done

--- app/javascript/components/Pages/Index.js
+++ app/javascript/components/Pages/Index.js
@@ -9,25 +9,57 @@ class Index extends Component {
             <React.Fragment>
                 <h3>All the Projects</h3>
                 <Row id="cards">
-                    { this.props.projects.map((project, index) => {
-                        return (
-                        <Col sm="4" key={ index }>
-                            <Card body>
-                            <CardTitle>
-                                <h5>{ project.name }</h5>
-                                <h5>{ project.description }</h5>
-                                <p>
-                                <NavLink to={`/project_show/${project.id}`}>
-                                    <Button color="secondary">
-                                    Show Page
-                                    </Button>
-                                </NavLink>
-                                </p>
-                            </CardTitle>
-                            </Card>
-                        </Col>
-                        )
-                    })}
+
+                    {/* If there are no filters selected, render all the Projects */}
+                    {this.props.currentFiltersSelected.length === 0 && 
+                    <>
+                        {this.props.projects.map((project, index) => {
+                            return (
+                            <Col sm="4" key={ index }>
+                                <Card body>
+                                <CardTitle>
+                                    <h5>{ project.name }</h5>
+                                    <h5>{ project.description }</h5>
+                                    <p>
+                                    <NavLink to={`/project_show/${project.id}`}>
+                                        <Button color="secondary">
+                                        Show Page
+                                        </Button>
+                                    </NavLink>
+                                    </p>
+                                </CardTitle>
+                                </Card>
+                            </Col>
+                            )
+                        })}
+                    </>
+                    }
+                    {/* If there are filters selected, render this.props.projectsToBeShown */}
+                    {this.props.currentFiltersSelected.length !== 0 && 
+                    // Draw data from projectsToBeShown instead of projects from props
+                    <>
+                        {this.props.projectsToBeShown.map((project, index) => {
+                            return (
+                            <Col sm="4" key={ index }>
+                                <Card body>
+                                <CardTitle>
+                                    <h5>{ project.name }</h5>
+                                    <h5>{ project.description }</h5>
+                                    <p>
+                                    <NavLink to={`/project_show/${project.id}`}>
+                                        <Button color="secondary">
+                                        Show Page
+                                        </Button>
+                                    </NavLink>
+                                    </p>
+                                </CardTitle>
+                                </Card>
+                            </Col>
+                            )
+                        })}
+                    </>
+                    }
+
                 </Row>
             </React.Fragment>
         )
