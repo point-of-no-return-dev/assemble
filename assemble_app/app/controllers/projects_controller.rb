aa62d250b99c86b42c83a1e3ada8eaee21371747
cynavago <cnavarrohdz@gmail.com>
2020-09-04 10:14:00 -0700

    currently working on creating the create project page, commiting so I can pull from origin master

--- assemble_app/app/controllers/projects_controller.rb
+++ assemble_app/app/controllers/projects_controller.rb
@@ -1,2 +1,17 @@
 class ProjectsController < ApplicationController
+
+    def create
+        project = Project.create(project_params)
+        if project.valid?
+            render json: project
+        else
+            render json: project.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def project_params
+        params.require(:project).permit(:name, :description)
+    end
+
 end
