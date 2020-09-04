3d85665429e28d2257c9dc9b73efa5a63e63af0c
cynavago <cnavarrohdz@gmail.com>
2020-09-04 16:05:34 -0700

    still working on filter component

--- assemble_app/app/javascript/components/Pages/Index.js
+++ assemble_app/app/javascript/components/Pages/Index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
-import Header from "../Components/Header.js"
+import Filter from "../Components/Filter.js"
 
 class Index extends Component {
     render() {
         return(
             <React.Fragment>
-                <p>Index Page</p>
+                <h2>CREATE. FIND. ASSEMBLE.</h2>
+                <h5>View projects below.</h5>
+                <Filter />
             </React.Fragment>
         )
     }
