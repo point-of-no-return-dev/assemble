3be3f5ea85c96286130985e89c791405a4c3c5a2
jlmooreiii <jloftonmooreiii@gmail.com>
2020-09-08 16:40:56 -0700

    edit button has conditional rendering

--- assemble_app/app/javascript/components/Pages/ProjectShow.js
+++ assemble_app/app/javascript/components/Pages/ProjectShow.js
@@ -1,14 +1,34 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { Button } from 'reactstrap';
+import { NavLink } from 'react-router-dom'
+
 class ProjectShow extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOwnerViewing: this.props.isCurrentUserTheOwner(this.props.project)
+        }
+    }
     render() {
+        console.log(this.props.isCurrentUserTheOwner(this.props.project))
         // deconstructs projects so I don't have to type out the entire dot syntax for it every time.
         let { project } = this.props
-        return(
+        return(    
             <React.Fragment>
                 <h2>{ project.name }</h2>
                 <h5>Project Description:</h5>
                 <p>{ project.description }</p>
 
+                {/* If the current user is the project owner, then the eti and delete buttons will be shown. */}
+                { this.state.isOwnerViewing &&
+                <>
+                    <NavLink to={`/project_edit/${project.id}`}>
+                        <Button color='secondary'>
+                            Edit
+                        </Button>
+                    </NavLink>
+                </>
+                }
             </React.Fragment>
         )
     }
