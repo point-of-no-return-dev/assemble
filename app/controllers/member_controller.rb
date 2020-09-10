85528a5cb14635e2c94a456666fecbe432bdd195
jlmooreiii <jloftonmooreiii@gmail.com>
2020-09-10 14:07:00 -0700

    finished create project functionality with Members table and the project-technologies table

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -2,5 +2,19 @@ class MemberController < ApplicationController
     def index
         members = Member.all
         render json: members
-    end    
+    end   
+    
+    def create
+        member = Member.create(member_params)
+        if project.valid?
+            render json: project
+        else
+            render json: project.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def member_params
+        params.require(:project).permit(:user_id, :project_id, :is_owner)
+    end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,24 @@
 class ProjectsController < ApplicationController
+    
     def index
         projects = Project.all
         render json: projects
     end
-
+    
     def create
-        project = Project.create(project_params)
+        project = Project.create(project_params["project"])
         if project.valid?
+            Member.create(user_id: current_user.id, project_id: project.id, is_owner: true)
+            project_params["projectTechnologies"].each do |attribute| 
+                ProjectTechnology.create(project_id: project.id, technology_id: attribute)
+            end
+
             render json: project
         else
             render json: project.errors, status: :unprocessable_entity
         end
+        
+        
     end
 
     def update
@@ -33,10 +41,8 @@ class ProjectsController < ApplicationController
         end
     end
 
-
-
     private
     def project_params
-        params.require(:project).permit(:name, :description)
+        params.permit(:project =>[:name, :description], :projectTechnologies => [])
     end
 end
