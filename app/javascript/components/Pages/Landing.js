ac27648d909045ff4ee732de6d9ca0cb6a6392e5
cynavago <cnavarrohdz@gmail.com>
2020-09-10 09:38:51 -0700

    styling for landing page, devise pages, create project, nav, and footer

--- app/javascript/components/Pages/Landing.js
+++ app/javascript/components/Pages/Landing.js
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
-import { Jumbotron, Button, Container, Row, Col } from 'reactstrap';
+import { 
+    Jumbotron,
+    Card,
+    CardDeck,
+    CardImg,
+    CardBody,
+    CardTitle,
+    Button, 
+    Container,
+    CardText,
+    Row, 
+    Col } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 
@@ -8,13 +19,14 @@ class Landing extends Component {
         return(
             <React.Fragment>
                 <div>
-                    <Jumbotron>
-                        <h1 className="display-3">CREATE.FIND.ASSEMBLE.</h1>
-                        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                        <p className="lead">
+                    <Jumbotron id="hero-image">
+                        <h1 id="h1-white">CREATE. FIND. ASSEMBLE.</h1>
+                        <p className="lead" id="hero-body">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information. This is a simple hero unit, a simple Jumbotron-style com.</p>
+                        <p>
+                            <br/>
                             {/* Button will take you to the index of projects */}
-                            <NavLink to={"/index"}>
-                                <Button color="primary">View Projects</Button>
+                            <NavLink id="center" to={"/index"}>
+                                <Button id="primary-btn" color="primary">View Projects</Button>
                             </NavLink>
                         </p>
                     </Jumbotron>
@@ -22,50 +34,72 @@ class Landing extends Component {
 
                 {/* //For Create Section */}
 
-                <div className="landing-section-header">
-                    <h3>CREATE.</h3>
-                </div>
-                <Container className="themed-container" fluid={true}>
-                    <Row xs="2">
-                        <Col>
-                            <p>About Create.</p>
-                        </Col>
-                        <Col>
-                            <img src="" alt = "Some image showing create" />
-                        </Col>
+
+                <Container>
+                    <Row>
+                        <CardDeck>
+                            <Col xs="6">
+                                <Card id="no-border">
+                                    <CardBody>
+                                        <h3 id="h3-dark-blue">CREATE.</h3>
+                                        <CardText id="section-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                            <Col xs="6">
+                                <Card id="no-border">
+                                    <CardImg id="section-image" src="https://i.ibb.co/HB84WpB/lp-create-1.png" alt="Card image cap" />
+                                </Card>
+                            </Col>
+                        </CardDeck>
                     </Row>
                 </Container>
 
 
                 {/* //For Find Secion */}
-                <div className="landing-section-header">
-                    <h3>FIND.</h3>
-                </div>
-                <Container className="themed-container" fluid={true}>
-                    <Row xs="2">
-                        <Col>
-                            <img src="" alt = "Some image showing create" />
-                        </Col>
-                        <Col>
-                            <p>About Find.</p>
-                        </Col>
+                <Container>
+                    <Row>
+                        <CardDeck>
+                            <Col xs="6">
+                                <Card id="no-border">
+                                    <CardImg id="section-image" src="https://i.ibb.co/QmFMGz6/lp-find-1.png" alt="Card image cap" />
+                                </Card>
+                            </Col>
+                            <Col xs="6">
+                                <Card id="no-border">
+                                    <CardBody>
+                                        <h3 id="h3-orange">FIND.</h3>
+                                        <CardText id="section-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </CardDeck>
                     </Row>
                 </Container>
 
+
+
                 {/* //For Assemble Section */}
-                <div className="landing-section-header">
-                    <h3>ASSEMBLE.</h3>
-                </div>
-                <Container className="themed-container" fluid={true}>
-                    <Row xs="2">
-                        <Col>
-                            <p>About Assemble.</p>
-                        </Col>
-                        <Col>
-                            <img src="" alt = "Some image showing create" />
-                        </Col>
+                <Container>
+                    <Row>
+                        <CardDeck>
+                            <Col xs="6">
+                                <Card id="no-border">
+                                    <CardBody>
+                                        <h3 id="h3-teal">ASSEMBLE.</h3>
+                                        <CardText id="section-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</CardText>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                            <Col xs="6">
+                                <Card id="no-border">
+                                    <CardImg id="section-image" src="https://i.ibb.co/vPcFGkN/lp-assemble-1.png" alt="Card image cap" />
+                                </Card>
+                            </Col>
+                        </CardDeck>
                     </Row>
                 </Container>
+
                 
             </React.Fragment>
         )
