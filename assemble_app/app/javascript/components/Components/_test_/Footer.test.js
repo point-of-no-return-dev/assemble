700df2db305380b2116229e3ff771d740d816609
jlmooreiii <jloftonmooreiii@gmail.com>
2020-09-11 15:50:32 -0700

    adding a couple of tests but need to switch to a new branch

--- assemble_app/app/javascript/components/Components/_test_/Footer.test.js
+++ assemble_app/app/javascript/components/Components/_test_/Footer.test.js
@@ -11,7 +11,26 @@ describe('<Footer />', () => {
         const p = wrapper.find("p");
         //Specifically pulls the text out of the <p> tag
         const result = p.text();
-        //Now that you successfully isolatd what you are testing for, you can write your expect statement.
+        //Now that you successfully isolated what you are testing for, you can write your expect statement.
         expect(result).toBe(" © 2020 Assemble.");     
     })
+    
+    it("should render 'About Us'", () => {
+        //This creates a shallow copy of the Footer (shallows means that any child componenets, if any won't be created, just the Footer)
+        const wrapper = shallow(<Footer />);
+        //Find the NavLink tag and saves the <NavLink> tag and everything in the p tag to a variable to be referenced in the next line
+        const NavLink = wrapper.find("NavLink");
+        //Specifically pulls the text out of the <NavLink> tag
+        const result = NavLink.text();
+        //Now that you successfully isolated what you are testing for, you can write your expect statement.
+        expect(result).toBe("About Us");
+    })
+    
+    it("should redirect to '/aboutus' when 'About Us' is clicked", () => {
+        const wrapper = shallow(<Footer />);
+        const NavLink = wrapper.find("NavLink");
+        NavLink.simulate("click");
+    })
+
+    
 })
